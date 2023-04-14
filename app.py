9e157e6b0a02277a295411799abfe77e25e3e6f7
Akshat-Tiwari <tiwariakshatwork@gmail.com>
2023-04-14 16:01:57 -0700

    Created Base Files

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    img = np.fromstring(file.read(), np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    emotions = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+    # add emotion recognition code here
+
+        emotion = emotions[result[0]]
+   
+    # add emotion recognition code here
+
+    return f"The predicted emotion is {emotion}"
+
+if __name__ == '__main__':
+    app.run(debug=True) 
